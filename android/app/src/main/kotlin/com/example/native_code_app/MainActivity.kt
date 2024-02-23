a910e87244bb181406953525d41ec7717c893145
鳥越幸樹 <niaoyuexingshu@gmail.com>
2024-02-23 17:44:14 +0900

    feat: swiftは完了、kotlinは未

--- android/app/src/main/kotlin/com/example/native_code_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_code_app/MainActivity.kt
@@ -1,6 +1,29 @@
-package com.example.native_code_app
+package com.example.flutter_native_sample
 
 import io.flutter.embedding.android.FlutterActivity
 
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "android_method_channel_experiment.example.com//native_feature"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+            if(call.method == "initializeNativeSetup"){
+                val isInitialized = initializeNativeSetup()
+                result.success(isInitialized)
+            }else{
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun initializeNativeSetup(): Boolean {
+        // Do some operation
+        return true;
+    }
 }
